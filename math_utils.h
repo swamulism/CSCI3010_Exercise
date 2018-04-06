c6a6c3a5febfaa7bd47943b37e94b7c04ec74732
pikawolfy <mckenzie.m.weller@gmail.com>
2018-04-06 16:05:39 -0600

    header file

--- math_utils.h
+++ math_utils.h
@@ -1,1 +1,16 @@
-//math_utils.h
+#include <vector>
+using namespace std;
+
+#ifndef _MATH_UTILS_H_
+#define _MATH_UTILS_H_
+
+class Math {
+public:
+	static bool IsSquare(int num);
+	static int GetDigit(int num, int place);
+	static bool EqualParity(int x, int y);
+	static bool EqualParity(vector<int> nums);
+private:
+}
+
+#endif
