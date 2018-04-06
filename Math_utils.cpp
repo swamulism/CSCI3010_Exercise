fce229e10e0420139cf4542686db915aa988b4a3
Joshua Griffiths <joshuagriffiths@engr2-31-101-dhcp.int.colorado.edu>
2018-04-06 16:10:45 -0600

    Finished IsSquare method

--- Math_utils.cpp
+++ Math_utils.cpp
@@ -1,1 +1,19 @@
 /* McKenzie Weller */
+//Joshua Griffiths 
+
+#include "math_utils.h"
+#include <cmath>
+using namespace std
+
+
+bool Math::IsSquare(int num){
+
+	sqr = sqrt(num);
+	
+	if ((sqr - floor(sqr)) == 0 {
+
+	return True;
+	}
+
+	else return False;
+}
